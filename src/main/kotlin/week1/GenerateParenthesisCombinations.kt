53f0735530bd89ad9bc1bc5513e7976835187d39
Daniel Lin <ephemient@gmail.com>
2021-12-02 12:46:40 -0500

    Week 1: Possible value parentheses

--- src/main/kotlin/week1/GenerateParenthesisCombinations.kt
+++ src/main/kotlin/week1/GenerateParenthesisCombinations.kt
@@ -1,4 +1,16 @@
 package week1
 
-fun generateParenthesisCombinations(num: Int): List<String> = TODO()
-
+fun generateParenthesisCombinations(num: Int): List<String> {
+    val cache = ArrayList<List<String>>(num)
+    cache.add(listOf(""))
+    (1..num).mapTo(cache) { i ->
+        (0 until i).flatMap { j ->
+            cache[j].flatMap { head ->
+                cache[i - j - 1].map { tail ->
+                    "($head)$tail"
+                }
+            }
+        }
+    }
+    return cache.last()
+}
