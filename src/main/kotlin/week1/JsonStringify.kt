de78655858863ab1bba372fd0b36a34d1968d53c
Daniel Lin <ephemient@gmail.com>
2021-12-02 11:54:03 -0500

    Week 1: JSON stringify

--- src/main/kotlin/week1/JsonStringify.kt
+++ src/main/kotlin/week1/JsonStringify.kt
@@ -1,5 +1,8 @@
 package week1
 
+import java.text.NumberFormat
+import java.util.Locale
+
 sealed class JsonElement
 data class JsonObject(val fields: Map<String, JsonElement>) : JsonElement() {
     constructor(vararg fields: Pair<String, JsonElement>) : this(fields.toMap())
@@ -14,5 +17,52 @@ data class JsonString(val value: String) : JsonElement()
 data class JsonBoolean(val value: Boolean) : JsonElement()
 object JsonNull : JsonElement()
 
-fun JsonElement.stringify(): String = TODO()
+fun JsonElement.stringify(): String = buildString { appendJson(this@stringify) }
 
+private val nf = NumberFormat.getInstance(Locale.ROOT).apply {
+    minimumFractionDigits = 0
+}
+
+private fun StringBuilder.appendJson(element: JsonElement) {
+    when (element) {
+        is JsonObject -> {
+            append('{')
+            var needsComma = false
+            for ((key, value) in element.fields) {
+                if (needsComma) append(',')
+                appendEscaped(key)
+                append(':')
+                appendJson(value)
+                needsComma = true
+            }
+            append('}')
+        }
+        is JsonArray -> {
+            append('[')
+            var needsComma = false
+            for (child in element.elements) {
+                if (needsComma) append(',')
+                appendJson(child)
+                needsComma = true
+            }
+            append(']')
+        }
+        is JsonNumber -> append(nf.format(element.value))
+        is JsonString -> appendEscaped(element.value)
+        is JsonBoolean -> append(element.value.toString())
+        JsonNull -> append("null")
+    }
+}
+
+private fun StringBuilder.appendEscaped(string: String) {
+    append('"')
+    for (char in string) {
+        if (char.isISOControl()) {
+            append("\\u%04X".format(char.code))
+        } else {
+            if (char == '"' || char == '\\') append('\\')
+            append(char)
+        }
+    }
+    append('"')
+}
